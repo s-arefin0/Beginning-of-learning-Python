96079db78bf19611390efaa7d10f9d6858942300
Sultanul Arefin <151627017+s-arefin0@users.noreply.github.com>
2023-11-23 13:35:49 +0600

    Add files via upload

---
+++ making_a_grading_system.py
@@ -0,0 +1,47 @@
+# Total marks greater than equal 80 then A+
+# Total marks greater than equal 70 and less than 80 then A
+# Total marks greater than equal 60 and less than 70 then A-
+# Otherwise F
+
+Total_marks = 75
+if Total_marks >= 80:
+    print("A+")
+elif Total_marks >= 70 and Total_marks < 80:
+    print("A")
+elif Total_marks >= 60 and Total_marks < 70:
+    print("A-")
+else:
+    print("F")
+    
+    
+Total_marks = 99
+if Total_marks >= 80:
+    print("A+")
+elif Total_marks >= 70 and Total_marks < 80:
+    print("A")
+elif Total_marks >= 60 and Total_marks < 70:
+    print("A-")
+else:
+    print("F")
+    
+
+Total_marks = 69
+if Total_marks >= 80:
+    print("A+")
+elif Total_marks >= 70 and Total_marks < 80:
+    print("A")
+elif Total_marks >= 60 and Total_marks < 70:
+    print("A-")
+else:
+    print("F")
+    
+
+Total_marks = 59
+if Total_marks >= 80:
+    print("A+")
+elif Total_marks >= 70 and Total_marks < 80:
+    print("A")
+elif Total_marks >= 60 and Total_marks < 70:
+    print("A-")
+else:
+    print("F")
--- use_of_conditions.py
+++ use_of_conditions.py
@@ -1,5 +1,20 @@
 Price_of_watch = 500
-
 if Price_of_watch == 500:
     print("I will buy a shirt inshaAllah")
     print("I will buy another pant too inshaAllah")
+    
+    
+Shirt_price = 600
+if Shirt_price == 500:
+    print("I will buy one InshaAllah")
+else:
+    print("I will return home InshaAllah")
+    print("Plan cancel")
+    
+    
+Price_of_a_cake = 400
+if Price_of_a_cake == 400:
+    print("buy me one")
+    print("execute the plan")
+else:
+    print("plan cancel")
