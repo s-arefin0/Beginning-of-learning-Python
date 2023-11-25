e860aa31808523c4cb70ad255e6bf172c3137408
Sultanul Arefin <151627017+s-arefin0@users.noreply.github.com>
2023-11-25 12:45:20 +0600

    Add files via upload

---
+++ declare_tupple.py
@@ -0,0 +1,30 @@
+# making tupples in different way
+
+# with bracket 
+Fruits = ("Apple", "Banana", "Grapes")
+print(type(Fruits))
+print(Fruits)
+
+# without bracket
+Fruits = "Apple", "Banana", "Grapes"
+print(type(Fruits))
+print(Fruits)
+
+# adding numbers, duplicate and other things
+Everything = ("Apple", "Banana", "Grapes", "Grapes", "Banana", 1, 2, 3 , ["s"], {'b'})
+print(type(Everything))
+print(Everything)
+
+#without bracket 
+Everything = "Apple", "Banana", "Grapes", "Grapes", "Banana", 1, 2, 3 , ["s"], {'b'}
+print(type(Everything))
+print(Everything)
+
+# single item in tupple
+Fruits = ("Apple",)
+print(type(Fruits))
+print(Fruits)
+
+Fruits = "Apple",
+print(type(Fruits))
+print(Fruits)
---
+++ string_variable.py
@@ -0,0 +1,5 @@
+user_name = "s-arefin0"
+print(type(user_name))
+
+password = "123456789"
+print(user_name, password)
