64378d5fa0def7c48987d7f1422d08feee442d45
Sultanul Arefin <151627017+s-arefin0@users.noreply.github.com>
2023-12-01 11:38:31 +0600

    Add files via upload

---
+++ accesing_individual_item_in_tupple.py
@@ -0,0 +1,23 @@
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[0])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[1])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[2])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[3])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[4])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[-1])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[-2])
+
+Stationaries = ("Pen", "Notebook", "Pencil", "Eraser", "Sharpner")
+print(Stationaries[-3])
